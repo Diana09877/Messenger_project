3b65985406a2b26f10b638146c3a0bfbdc29f478
Diana <diana09877@mail.com>
2025-06-17 08:04:21 +0600

    Added MessengerProject.pythonanywhere.com to ALLOWED_HOSTS for deployment

--- messenger_project/settings.py
+++ messenger_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-@e2b53ebx54%pr$mc((c7sazlfjry*#yag@wn%kskpm5kq)o=$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'MessengerProject.pythonanywhere.com']
+
 
 
 # Application definition
